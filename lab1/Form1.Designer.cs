b1266990313e5fc6e24d2aab4ed7de2eac8e6653
MrFutureHome <nikolamelnik222@gmail.com>
2024-10-02 23:24:15 +0300

    Создан интерфейс программы

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -28,37 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // label1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(560, 185);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(641, 125);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество переменных";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(671, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество ограничений";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(670, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 29);
+            this.textBox2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2010, 1238);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(1440, 1100);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -12,9 +12,180 @@ namespace lab1
 {
     public partial class Form1 : Form
     {
+        private Panel variablesPanel;
+        private Panel constraintsPanel;
+        private const int Padding = 10; // Отступы между элементами
+        private const int ElementHeight = 25;
+        private const int MinWidth = 50;
+
+
         public Form1()
         {
             InitializeComponent();
+            // Установка размеров и свойств формы
+            this.Text = "Симплекс-Калькулятор";
+            //this.Size = new Size(800, 600);
+
+            // Панель для переменных
+            variablesPanel = new Panel
+            {
+                Location = new Point(10, 100),
+                AutoSize = true,
+                AutoSizeMode = AutoSizeMode.GrowAndShrink
+            };
+
+            // Панель для ограничений
+            constraintsPanel = new Panel
+            {
+                Location = new Point(10, 250),
+                AutoSize = true,
+                AutoSizeMode = AutoSizeMode.GrowAndShrink
+            };
+
+            // Добавляем панели на форму
+            this.Controls.Add(variablesPanel);
+            this.Controls.Add(constraintsPanel);
+
+            // Добавляем события для автоматического обновления при изменении значений
+            textBox1.TextChanged += TextBoxes_TextChanged;
+            textBox2.TextChanged += TextBoxes_TextChanged;
+        }
+
+        private void TextBoxes_TextChanged(object sender, EventArgs e)
+        {
+            // Очищаем панели перед созданием новых элементов
+            variablesPanel.Controls.Clear();
+            constraintsPanel.Controls.Clear();
+
+            // Используем существующие textbox1 и textbox2
+            if (int.TryParse(textBox1.Text, out int numVariables) && int.TryParse(textBox2.Text, out int numConstraints))
+            {
+                CreateVariableInputs(numVariables);
+                CreateConstraintInputs(numConstraints, numVariables);
+            }
+            else
+            {
+                //MessageBox.Show("Введите корректные числа для переменных и ограничений.");
+                return;
+            }
+        }
+
+        private void CreateVariableInputs(int numVariables)
+        {
+            Label objectiveLabel = new Label
+            {
+                Text = "Целевая функция:",
+                AutoSize = true,
+                Location = new Point(10, 10)
+            };
+            variablesPanel.Controls.Add(objectiveLabel);
+
+            // Рассчитываем ширину полей в зависимости от количества переменных и доступного места
+            int panelWidth = this.ClientSize.Width - 40;
+            int totalWidth = panelWidth - (Padding * 2 * numVariables);
+            int fieldWidth = Math.Max(MinWidth, totalWidth / numVariables);
+
+            for (int i = 0; i < numVariables; i++)
+            {
+                TextBox variableTextBox = new TextBox
+                {
+                    Width = fieldWidth,
+                    Location = new Point(i * (fieldWidth + Padding * 2), 40)
+                };
+
+                Label variableLabel = new Label
+                {
+                    Text = $"x{i + 1}",
+                    AutoSize = true,
+                    Location = new Point(variableTextBox.Location.X + fieldWidth, 44) // Метка справа от TextBox
+                };
+
+                variablesPanel.Controls.Add(variableTextBox);
+                variablesPanel.Controls.Add(variableLabel);
+            }
+
+            // Радиокнопки для выбора типа задачи (максимизация/минимизация)
+            RadioButton maxRadioButton = new RadioButton
+            {
+                Text = "Максимизация",
+                Location = new Point(10, 70),
+                AutoSize = true
+            };
+            RadioButton minRadioButton = new RadioButton
+            {
+                Text = "Минимизация",
+                Location = new Point(120, 70),
+                AutoSize = true
+            };
+            minRadioButton.Checked = true; // По умолчанию минимизация
+
+            variablesPanel.Controls.Add(maxRadioButton);
+            variablesPanel.Controls.Add(minRadioButton);
+        }
+
+        private void CreateConstraintInputs(int numConstraints, int numVariables)
+        {
+            Label constraintsLabel = new Label
+            {
+                Text = "Ограничения:",
+                AutoSize = true,
+                Location = new Point(10, 10)
+            };
+            constraintsPanel.Controls.Add(constraintsLabel);
+
+            int panelWidth = this.ClientSize.Width - 40;
+            int totalWidth = panelWidth - (Padding * 2 * (numVariables + 2));
+            int fieldWidth = Math.Max(MinWidth, totalWidth / (numVariables + 2));
+
+            // Создаем строку для каждого ограничения
+            for (int i = 0; i < numConstraints; i++)
+            {
+                int yPosition = 40 + i * (ElementHeight + Padding); // Вычисляем Y-позицию для каждого ограничения
+
+                // Создаем набор текстовых полей и меток для каждого ограничения
+                for (int j = 0; j < numVariables; j++)
+                {
+                    TextBox variableTextBox = new TextBox
+                    {
+                        Width = fieldWidth,
+                        Location = new Point(j * (fieldWidth + Padding * 2), yPosition)
+                    };
+
+                    Label variableLabel = new Label
+                    {
+                        Text = $"x{j + 1}",
+                        AutoSize = true,
+                        Location = new Point(variableTextBox.Location.X + fieldWidth, yPosition + 4) // Метка справа от TextBox
+                    };
+
+                    constraintsPanel.Controls.Add(variableTextBox);
+                    constraintsPanel.Controls.Add(variableLabel);
+                }
+
+                // Комбобокс для типа ограничения
+                ComboBox constraintType = new ComboBox
+                {
+                    Width = fieldWidth,
+                    Location = new Point(numVariables * (fieldWidth + Padding * 2), yPosition),
+                    DropDownStyle = ComboBoxStyle.DropDownList
+                };
+                constraintType.Items.AddRange(new[] { "<=", ">=", "=" });
+                constraintType.SelectedIndex = 0;
+                constraintsPanel.Controls.Add(constraintType);
+
+                // Поле для ввода правой части уравнения
+                TextBox rightSide = new TextBox
+                {
+                    Width = fieldWidth,
+                    Location = new Point((numVariables + 1) * (fieldWidth + Padding * 2), yPosition)
+                };
+                constraintsPanel.Controls.Add(rightSide);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
